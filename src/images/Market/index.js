50cddb4b22fac452850a6cf353c7321ec262724e
Xiaohu Shu <sxh20150609@gmail.com>
2021-01-27 17:27:21 +0200

    change the explain pictures

--- src/images/Market/index.js
+++ src/images/Market/index.js
@@ -1,9 +1,9 @@
-import EUEuroStoxx from "./EU Euro Stoxx 50.png";
-import JapanNikkei225 from "./Japan Nikkei 225.png";
-import MSCIEMUSD from "./MSCI EM USD.png";
-import UKFTSE100 from "./UK FTSE 100.png";
-import USNasdaq100 from "./US Nasdaq 100.png";
-import USSP500 from "./US S&P 500.png";
+import EUEuroStoxx from "./shap/explicability_2021-01-26/avg_best10_shap_contrib_EU Euro Stoxx 50_2021-01-26.png";
+import JapanNikkei225 from "./shap/explicability_2021-01-26/avg_best10_shap_contrib_Japan Nikkei 225_2021-01-26.png";
+import MSCIEMUSD from "./shap/explicability_2021-01-26/avg_best10_shap_contrib_MSCI EM USD_2021-01-26.png";
+import UKFTSE100 from "./shap/explicability_2021-01-26/avg_best10_shap_contrib_UK FTSE 100_2021-01-26.png";
+import USNasdaq100 from "./shap/explicability_2021-01-26/avg_best10_shap_contrib_US Nasdaq 100_2021-01-26.png";
+import USSP500 from "./shap/explicability_2021-01-26/avg_best10_shap_contrib_US S&P 500_2021-01-26.png";
 import MarketRisk from "./market-risk.jpeg";
 
 export {
